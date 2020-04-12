2f5e23425e7abe6df158f0579f464ad12e40e877
aklu <amandaklusmeyer@gmail.com>
2020-04-12 17:01:22 -0500

    initial files for duly-noted-api

---
+++ main.js
@@ -0,0 +1,27 @@
+import Apollo from "apollo-server";
+import importAsString from "import-as-string";
+const schema = importAsString("./schema.graphql");
+
+const { ApolloServer } = Apollo;
+
+const resolvers = {
+    Mutation: {
+        echo(_, args) {
+            return args.str;
+        }
+    },
+    Query: {
+        ping() {
+            return "pong";
+        }
+    }
+};
+
+const server = new ApolloServer ({
+    resolvers,
+    typeDefs: schema
+});
+
+server.listen({ port: 3030 }).then((result) => {
+    console.log(`Server listening at ${result.url}`);
+});
