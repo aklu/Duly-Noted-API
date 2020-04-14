ee932efefa96c619fd4671ffe80665e3c337b292
aklu <amandaklusmeyer@gmail.com>
2020-04-14 10:32:03 -0500

    updated the rest of the changes for week 10

--- resolvers.js
+++ resolvers.js
@@ -8,7 +8,7 @@ export default {
       const { note } = args;
 
       const newNote = { ...note };
-
+ 
       if (!newNote.id) {
         newNote.id = cuid();
       }
@@ -28,8 +28,58 @@ export default {
       }
 
       savedNotes.push(newNote);
-
       return newNote;
+    },
+    updateNote(_, args) {
+      const { note } = args;
+
+      const updateNote = { ...note };
+
+      if (args.id !== note.id) {
+        throw new Error('The id for this note did not match any id in the database');
+      }
+
+      if (args.note.isArchived === "boolean")  {
+        updateNote.isArchived = args.note.isArchived;
+        const now = new Date();
+        updateNote.updatedAt = now.toISOString();      
+      }
+
+      if (args.note.isArchived !== "boolean")  {
+     }
+
+      if (typeof args.note.text === String) {
+        updateNote.text = args.note.text;
+        now = new Date();
+        updateNote.updatedAt = now.toISOString();    
+      }
+
+      if (typeof args.note.text !== String) {
+      }
+
+      savedNotes.find((note) => {
+        if(note.id === args.id){
+          note.isArchived = updateNote.isArchived;
+          note.text = updateNote.text;
+          note.updateAt = updateNote.updateAt;
+        }
+      });
+
+      return updateNote;
+    },
+    deleteNote(_, args){
+      const { note } = args;
+
+      const deleteNote = { ...note };
+
+      if (args.id !== deleteNote.id) {
+        throw new Error('The id for this note did not match any id in the database');
+      }
+
+      const deletedNotes = notes.filter((note) => note.id !== deleteNote.id);
+      savedNotes = deletedNotes;
+
+      return deleteNote;
     }
   },
   Query: {
@@ -37,6 +87,12 @@ export default {
       return savedNoted.find((note) => note.id === args.id);
     },
     notes() {
+      if(isArchived !== "boolean" || isArchived === false){
+        savedNotes = notes.filter((note) => note.isArchived !== true);
+      }
+      if (showArchive) {
+        savedNotes;
+      }
       return savedNotes;
     }
   }
