ce951b7017c5a1736f934dfa149a6cd8926c8889
aklu <amandaklusmeyer@gmail.com>
2020-04-21 18:35:55 -0500

    updates

--- notes.js
+++ notes.js
@@ -1,27 +1,21 @@
 import { promises as fs } from "fs";
 
-let notes = [];
-
 export async function saveNotes(notes){
-    const fs = require('fs');
-
-    //convert notes to json
-    let jsonNotes = JSON.stringify(notes);
-
+    
     //add information to file, append creates file if the file doesn't exist
     try {
-        fs.appendFile("./data.json", jsonNotes);
+        fs.appendFile("data.json", JSON.stringify(data), (err) => {
+            console.log(err)
+        });
     } catch (err){
-        return;
     }
-
 };
 
 export async function getNotes(){ 
     let jsonContent = [];
     //check if data.json file exists. If it does, it will parse the json into an array. If not it will return an empty jsonContent array
     try {
-        let contents = fs.readFileSync("/data.json");
+        let contents = fs.readFile("data.json");
         jsonContent = JSON.parse(contents);
         return jsonContent;
     }catch (err){
